ae66d63539cc8da94b88a4382721004a87555f52
Billy Mahmood <billy124@msn.com>
2015-11-26 22:43:26 +0000

    update billy to b

--- billy.sh
+++ billy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-function billy() {
-    if ! fn_exists "b_$1"; then
-        echo "billy does not except $1 as a pram"
-    else
-        "b_$1" "${@:2}"
+## The roof of everything
+function b() {
+    if ! fn_exists "b_$1"; 
+        then
+            echo "billy does not except $1 as a pram"
+        else
+            "b_$1" "${@:2}"
     fi
 }
 
+## check if a function exists
 fn_exists() {
     # appended double quote is an ugly trick to make sure we do get a string -- if $1 is not a known command, type does not output anything
     [ `type -t $1`"" == 'function' ]
