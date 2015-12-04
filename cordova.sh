569190d187e16058a4a9e092cf4247e66479bf0b
Billy Mahmood <billy@e3creative.co.uk>
2015-12-04 15:24:04 +0000

    add the serve function for laravel, ionic, cordova

--- cordova.sh
+++ cordova.sh
@@ -3,9 +3,12 @@
 function getPlatform() {
     COMMAND="cordova"
 
-    if [ -f $(PWD)/ionic.project ]; 
+    if [ -f $(PWD)/ionic.project ]
         then
             COMMAND="ionic"
+    elif [ -f $(PWD)/artisan ]
+        then
+            COMMAND="php artisan"
     fi
     
     echo $COMMAND
@@ -33,4 +36,9 @@ function b_build() {
 
     $(getPlatform) build $PLATFORM
     echo "command executed - $(getPlatform) build $PLATFORM"    
+}
+
+function b_serve() {
+    $(getPlatform) serve
+    echo "command executed - $(getPlatform) serve"   
 }
